13a0d071886e71c59cd885371f21d8ff999fa079
Tayyab Zain <zaint2755@gmail.com>
2026-08-18 17:11:13 +0500

    5.1 Scratch.gpt

--- Chapter 5/setup.py
+++
@@ -1,8 +0,0 @@
-import torch
-import accelerate
-import bitsandbytes
-
-print("PyTorch:", torch.__version__)
-print("CUDA available:", torch.cuda.is_available())
-print("Accelerate:", accelerate.__version__)
-print("bitsandbytes:", bitsandbytes.__version__)
