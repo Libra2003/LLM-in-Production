4c0266e7f3d760c79b239eac441c99fb028925a8
Tayyab Zain <zaint2755@gmail.com>
2026-08-19 22:28:25 +0500

    5.2

---
+++ Chapter 5/5.2.finetuning.py
@@ -0,0 +1,99 @@
+import os
+from transformers import(
+    GPT2Tokenizer,
+    GPT2LMHeadModel,
+    GPT2Config,
+    DataCollatorForLanguageModeling,
+    TrainingArguments,
+    Trainer,
+)
+from datasets import load_dataset
+
+# Load and format the dataset
+dataset = load_dataset("text", data_files="./data/crimeandpunishment.txt")
+dataset = dataset.filter(lambda sentence: len(sentence["text"]) > 1)
+print(dataset["train"][0])
+
+# Create model directory to save to
+model_dir = "./models/betterGPT/"
+if not os.path.exists(model_dir):
+    os.makedirs(model_dir)
+
+# Establish our GPT2 parameters (different from the paper and scratchGPT)
+config = GPT2Config(
+    vocab_size=50261,
+    n_positions=256,
+    n_embd=768,
+    activation_function="gelu",
+)
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+special_tokens_dict = {
+    "bos_token": "<BOS>",
+    "eos_token": "<EOS>",
+    "pad_token": "<PAD>",
+    "mask_token": "<MASK>"    
+}
+tokenizer.add_special_tokens(special_tokens_dict)
+
+# Instantiate our model from the config
+model = GPT2LMHeadModel.from_pretrained(
+    "gpt2", config=config, ignore_mismatched_sizes=True
+)
+
+# Create a tokenize function
+def tokenize(batch):
+    return tokenizer(
+        batch["text"], padding="max_length", truncation=True, max_length=256
+    )
+
+# tokenize our whole dataset (so we never have to do it again)
+tokenized_dataset = dataset.map(tokenize, batched=False)
+print(f"Tokenized: {tokenized_dataset['train'][0]}")
+
+# Create a data collator to format the data for training
+data_collector = DataCollatorForLanguageModeling(
+    tokenizer=tokenizer, mlm=False
+)# Masked Language Modeling - adds <MASK> tokens to guess the words
+
+# Establish training arguments
+train_args = TrainingArguments(
+    output_dir=model_dir,
+    num_train_epochs=1,
+    per_device_train_batch_size=8,
+    save_steps=5000,
+    save_total_limit=2,
+    report_to="none",
+)
+
+# Instantiate the Trainer
+trainer = Trainer(
+    model=model,
+    args=train_args,
+    data_collator=data_collector,
+    train_dataset=tokenized_dataset["train"]
+)
+
+# Train and save the model
+trainer.train()
+trainer.save_model(model_dir)
+tokenizer.save_pretrained(model_dir)
+
+# Load the saved model
+model = GPT2LMHeadModel.from_pretrained(model_dir)
+
+# Test the saved model
+input = "To be or not"
+tokenized_input = tokenizer(input, return_tensors="pt")
+out = model.generate(
+    input_ids = tokenized_input["input_ids"],
+    attention_mask=tokenized_input["attention_mask"],
+    max_length=256,
+    do_sample=True,
+    num_beams=5,
+    temperature=0.7,
+    top_k=50,
+    top_p=0.90,
+    no_repeat_ngram_size=2,
+)
+print(tokenizer.decode(out[0], skip_special_tokens=True))
